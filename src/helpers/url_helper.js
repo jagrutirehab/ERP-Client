cc1383d5b87eaf5a72edd114eaba5cca516b8bda
Bishal Sarkar <bishalsarkar.ml@gmail.com>
2025-08-18 13:18:22 +0530

    update

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -237,6 +237,20 @@ export const UPDATE_TAX = "/tax/update";
 //HUBSPOT
 export const GET_HUBSPOT_CONTACTS = "/hubspot/getContacts";
 
+//New Microservice APIS
+export const CSRF = "/csrf-token"
+export const MICRO_SIGN_IN = "/userauths/signin"
+export const MICRO_SIGN_UP = "/userauths/signup";
+export const MICRO_FORGOTT = "/userauths/forgott-password";
+export const MICRO_LOGOUT="/userauths/logout";
+export const CHANGE_PASSWORD="/userauths/forgott-password"
+export const USER = "/user"
+export const MOVE_TO_BIN="/user/move-recyclebin"
+export const ACTIVATE_DEACTIVATE_USER="/user/deactive"
+export const CHANGE_USER_PASSWORD="/user/change-password"
+export const EDIT_SELF="/user/edit-self";
+export const USER_ACTIVITY="/activity";
+export const GET_USER_ROLES="/user/roles";
 //Roles
 export const ROLES = "/role";
 
