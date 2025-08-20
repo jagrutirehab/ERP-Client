da12332aaf493ee640606943c4d822c5b526f80b
Bishal Sarkar <bishalsarkar.ml@gmail.com>
2025-08-20 10:51:28 +0530

    update in forms

---
+++ src/pages/Patient/Modals/Admissionform.modal.js
@@ -0,0 +1,152 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CustomModal from "../../../Components/Common/Modal";
+
+// Formik Validation
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+//flatpicker
+import Flatpicker from "react-flatpickr";
+import "flatpickr/dist/themes/material_green.css";
+
+//redux
+import { connect, useDispatch } from "react-redux";
+import {
+  admitDischargePatient,
+  dischargeIpdPatient,
+} from "../../../store/actions";
+import { Button, Col, Form, FormFeedback, Input, Label, Row } from "reactstrap";
+import { DISCHARGE_PATIENT } from "../../../Components/constants/patient";
+
+const AdmissionFormModal = ({ isOpen, patient }) => {
+  const dispatch = useDispatch();
+
+  const validation = useFormik({
+    enableReinitialize: true,
+
+    initialValues: {
+      patientId: patient?._id,
+      addmissionId: patient?.addmission?._id,
+      dischargeDate: "",
+    },
+    validationSchema: Yup.object({
+      dischargeDate: Yup.date().required(
+        "Please select discharge date and time"
+      ),
+    }),
+    onSubmit: (values) => {
+      const dischargeDate = new Date(values.dischargeDate);
+      const formattedDate = `${dischargeDate
+        .getDate()
+        .toString()
+        .padStart(2, "0")} ${dischargeDate.toLocaleString("default", {
+        month: "short",
+      })} ${dischargeDate.getFullYear()} ${dischargeDate
+        .getHours()
+        .toString()
+        .padStart(2, "0")}:${dischargeDate
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")}`;
+
+      dispatch(
+        dischargeIpdPatient({ ...values, dischargeDate: formattedDate })
+      );
+      validation.resetForm();
+      toggle();
+    },
+  });
+
+  const toggle = () =>
+    dispatch(admitDischargePatient({ data: null, isOpen: "" }));
+
+  return (
+    <React.Fragment>
+      <CustomModal
+        isOpen={isOpen === DISCHARGE_PATIENT}
+        title={"Discharge Patient"}
+        toggle={toggle}
+      >
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            validation.handleSubmit();
+            return false;
+          }}
+          className="needs-validation"
+          action="#"
+        >
+          <Row>
+            <Col>
+              <div className="mb-3">
+                <Label htmlFor="dischargeDate" className="form-label">
+                  Discharge Date & Time
+                </Label>
+                <Flatpicker
+                  name="dischargeDate"
+                  value={validation.values.dischargeDate || ""}
+                  onChange={([date]) => {
+                    const event = {
+                      target: { name: "dischargeDate", value: date },
+                    };
+                    validation.handleChange(event);
+                  }}
+                  options={{
+                    enableTime: true,
+                    time_24hr: true,
+                    dateFormat: "d M Y H:i",
+                    disable: [
+                      {
+                        from: "1900-01-01",
+                        to: patient?.addmission?.addmissionDate || new Date(),
+                      },
+                    ],
+                    enable: [
+                      function (date) {
+                        return patient?.addmission?.addmissionDate
+                          ? date > new Date(patient?.addmission?.addmissionDate)
+                          : false;
+                      },
+                    ],
+                  }}
+                  onBlur={validation.handleBlur}
+                  className="form-control shadow-none bg-light"
+                  id="dischargeDate"
+                />
+                {validation.touched.dischargeDate &&
+                validation.errors.dischargeDate ? (
+                  <FormFeedback className="d-block" type="invalid">
+                    {validation.errors.dischargeDate}
+                  </FormFeedback>
+                ) : null}
+              </div>
+            </Col>
+            <Col xs={12} className="mt-3">
+              <div className="d-flex gap-3 justify-content-end">
+                <Button onClick={toggle} size="sm" color="danger" type="button">
+                  Cancel
+                </Button>
+                <Button size="sm" type="submit">
+                  Save
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Form>
+      </CustomModal>
+    </React.Fragment>
+  );
+};
+
+AdmissionFormModal.propTypes = {
+  isOpen: PropTypes.string,
+  patient: PropTypes.object,
+};
+
+const mapStateToProps = (state) => ({
+  isOpen: state.Patient.admitDischargePatient?.isOpen,
+  patient: state.Patient.patient,
+});
+
+export default connect(mapStateToProps)(AdmissionFormModal);
--- src/pages/Patient/Views/AdmissionForms/AdmissionForms.js
+++ src/pages/Patient/Views/AdmissionForms/AdmissionForms.js
@@ -14,8 +14,37 @@ import IndipendentOpinion1 from "./IndipendentOpinion1";
 import IndipendentOpinion2 from "./IndipendentOpinion2";
 import IndipendentOpinion3 from "./IndipendentOpinion3";
 import ECTConsentForm from "./ECTConsentForm";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionItem,
+  Button,
+  Placeholder,
+  Row,
+  UncontrolledTooltip,
+} from "reactstrap";
+import GeneralCard from "../Components/GeneralCard";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import AdmissionformModal from "../../Modals/Admissionform.modal";
 
 const AddmissionForms = () => {
+  const patient = useSelector((state) => state.Patient.patient);
+  const [admission, setAdmission] = useState(null);
+  const [dateModal, setDateModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const toggleModal = () => setDateModal(!dateModal);
+  const [addmissionId, setAddmissionId] = useState();
+
+  const [open, setOpen] = useState("0");
+  const toggleAccordian = (id) => {
+    if (open === id) {
+      setOpen();
+    } else {
+      setOpen(id);
+    }
+  };
+
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
@@ -23,26 +52,118 @@ const AddmissionForms = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Page1 register={register} />
-      <Page2 register={register} />
-      <SeriousnessConsent register={register} />
-      <MediactionConcent register={register} />
-      <Admissionpage1 register={register} />
-      <Admissionpage2 register={register} />
-      <IndependentAdmAdult register={register} />
-      <IndependentAdmMinor register={register} />
-      <AdmWithHighSupport register={register} />
-      <DischargeIndependentAdult register={register} />
-      <DischargeIndependentMinor register={register} />
-      <IndipendentOpinion1 register={register} />
-      <IndipendentOpinion2 register={register} />
-      <IndipendentOpinion3 register={register} />
-      <ECTConsentForm register={register} />
-      <div style={{ textAlign: "center", margin: "20px" }}>
-        <button type="submit">Submit All</button>
+    <>
+      <div style={{ marginTop: "4rem" }}>
+        <Row className="timeline-right row-gap-5">
+          {[1].map((test, idx) => (
+            <GeneralCard key={idx} data="Admission Form">
+              <div
+                style={{ width: "100%" }}
+                className="d-flex  align-items-center justify-content-between"
+              >
+                <div
+                  style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  {patient.isAdmit === true && (
+                    <Button
+                      onClick={() => {
+                        toggleModal();
+                      }}
+                      size="sm"
+                      color="primary"
+                      className="mr-10"
+                    >
+                      Create new Admission
+                    </Button>
+                  )}
+                </div>
+
+                <div className="d-flex align-items-center">
+                  <UncontrolledTooltip
+                    placement="bottom"
+                    target={`expand-test-${idx}`}
+                  >
+                    Expand/Collapse
+                  </UncontrolledTooltip>
+                  <Button
+                    id={`expand-test-${idx}`}
+                    onClick={() => {
+                      toggleAccordian(idx.toString());
+                      setAddmissionId(test?._id);
+                    }}
+                    size="sm"
+                    outline
+                  >
+                    <i
+                      className={`${
+                        open === idx.toString()
+                          ? "ri-arrow-up-s-line"
+                          : "ri-arrow-down-s-line"
+                      } fs-6`}
+                    ></i>
+                  </Button>
+                </div>
+              </div>
+
+              {/* ACCORDION */}
+              <Accordion
+                className="timeline-date w-100"
+                open={open}
+                toggle={toggleAccordian}
+              >
+                <AccordionItem className="patient-accordion-item">
+                  <AccordionBody
+                    className="patient-accordion border-0"
+                    accordionId={idx.toString()}
+                  >
+                    {loading ? (
+                      <Placeholder />
+                    ) : (
+                      <div>
+                        <div className="timeline-2">
+                          <div className="timeline-continue">
+                            <Row className="timeline-right"></Row>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </AccordionBody>
+                </AccordionItem>
+              </Accordion>
+            </GeneralCard>
+          ))}
+        </Row>
       </div>
-    </form>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
+        <Page1 register={register} />
+        <Page2 register={register} />
+        <SeriousnessConsent register={register} />
+        <MediactionConcent register={register} />
+        <Admissionpage1 register={register} />
+        <Admissionpage2 register={register} />
+        <IndependentAdmAdult register={register} />
+        <IndependentAdmMinor register={register} />
+        <AdmWithHighSupport register={register} />
+        <DischargeIndependentAdult register={register} />
+        <DischargeIndependentMinor register={register} />
+        <IndipendentOpinion1 register={register} />
+        <IndipendentOpinion2 register={register} />
+        <IndipendentOpinion3 register={register} />
+        <ECTConsentForm register={register} />
+        <div style={{ textAlign: "center", margin: "20px" }}>
+          <button type="submit">Submit All</button>
+        </div>
+      </form> */}
+      <AdmissionformModal
+        isOpen={dateModal}
+        toggle={toggleModal}
+        admission={admission}
+      />
+    </>
   );
 };
 
