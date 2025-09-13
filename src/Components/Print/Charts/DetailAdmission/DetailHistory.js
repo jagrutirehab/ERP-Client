f4284c3d7fa2e0c51824d2967a97db8b3b458629
pratyush Singha <pratyushsingha83@gmail.com>
2025-09-13 13:29:46 +0530

    fix prescription print

--- src/Components/Print/Charts/DetailAdmission/DetailHistory.js
+++ src/Components/Print/Charts/DetailAdmission/DetailHistory.js
@@ -41,7 +41,7 @@ const DetailHistory = ({ data, styles }) => {
               ...styles.w50,
             }}
           >
-            <Text style={{ ...styles.fontMd, ...styles.textUppercase }}>
+            <Text style={{ ...styles.fontMd, ...styles.textCapitalize }}>
               informant{" "}
             </Text>
           </View>
@@ -55,7 +55,7 @@ const DetailHistory = ({ data, styles }) => {
                 ...styles.mrgnBottom5,
               }}
             >
-              <Text style={{ ...styles.fontMd, ...styles.textUppercase }}>
+              <Text style={{ ...styles.fontMd, ...styles.textCapitalize }}>
                 Self{" "}
               </Text>
               <Text style={{ ...styles.fontMd, ...styles.textCapitalize }}>
@@ -69,10 +69,10 @@ const DetailHistory = ({ data, styles }) => {
                 ...styles.justifyBetween,
               }}
             >
-              <Text style={{ ...styles.fontMd, ...styles.textUppercase }}>
+              <Text style={{ ...styles.fontMd, ...styles.textCapitalize }}>
                 {data.adequate}
               </Text>
-              <Text style={{ ...styles.fontMd, ...styles.textUppercase }}>
+              <Text style={{ ...styles.fontMd, ...styles.textCapitalize }}>
                 {data.reliable || ""}
               </Text>
             </View>
@@ -83,19 +83,25 @@ const DetailHistory = ({ data, styles }) => {
             <Text style={styles.fontSize13}>
               History / Onset Duration & Progress:
             </Text>
-            <Text style={styles.preText}>{data?.history || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.history || ""}
+            </Text>
           </View>
         )}
         {data?.negativeHistory && (
           <View style={styles.mrgnBottom10} wrap={false}>
             <Text style={styles.fontSize13}>Negative History:</Text>
-            <Text style={styles.preText}>{data?.negativeHistory || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.negativeHistory || ""}
+            </Text>
           </View>
         )}
         {data?.pastHistory && (
           <View style={styles.mrgnBottom10} wrap={false}>
             <Text style={styles.fontSize13}>Past History:</Text>
-            <Text style={styles.preText}>{data?.pastHistory || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.pastHistory || ""}
+            </Text>
           </View>
         )}
         {data?.developmentHistory && (
@@ -103,19 +109,25 @@ const DetailHistory = ({ data, styles }) => {
             <Text style={styles.fontSize13}>
               Development History & Childhood/Adolescence:
             </Text>
-            <Text style={styles.preText}>{data?.developmentHistory || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.developmentHistory || ""}
+            </Text>
           </View>
         )}
         {data?.occupationHistory && (
           <View style={styles.mrgnBottom10} wrap={false}>
             <Text style={styles.fontSize13}>Occupation History:</Text>
-            <Text style={styles.preText}>{data?.occupationHistory || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.occupationHistory || ""}
+            </Text>
           </View>
         )}
         {data?.familyHistory && (
           <View style={styles.mrgnBottom10} wrap={false}>
             <Text style={styles.fontSize13}>Family History:</Text>
-            <Text style={styles.preText}>{data?.familyHistory || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.familyHistory || ""}
+            </Text>
           </View>
         )}
         {data?.personalHistory && (
@@ -123,19 +135,25 @@ const DetailHistory = ({ data, styles }) => {
             <Text style={styles.fontSize13}>
               Personal / Sexual / Marital History:
             </Text>
-            <Text style={styles.preText}>{data?.personalHistory || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.personalHistory || ""}
+            </Text>
           </View>
         )}
         {data?.personality && (
           <View style={styles.mrgnBottom10} wrap={false}>
             <Text style={styles.fontSize13}>Personality:</Text>
-            <Text style={styles.preText}>{data?.personality || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.personality || ""}
+            </Text>
           </View>
         )}
         {data?.socialSupport && (
           <View style={styles.mrgnBottom10} wrap={false}>
             <Text style={styles.fontSize13}>Social Support:</Text>
-            <Text style={styles.preText}>{data?.socialSupport || ""}</Text>
+            <Text style={{ ...styles.preText, ...styles.textCapitalize }}>
+              {data?.socialSupport || ""}
+            </Text>
           </View>
         )}
       </View>
