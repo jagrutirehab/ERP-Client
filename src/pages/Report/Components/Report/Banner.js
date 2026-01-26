40014c412c58455dea26687d2d5c20f8daa79a17
pratyush Singha <pratyushsingha83@gmail.com>
2026-01-26 13:03:09 +0530

    fix report

--- src/pages/Report/Components/Report/Banner.js
+++ src/pages/Report/Components/Report/Banner.js
@@ -1,23 +1,32 @@
 import React from "react";
 import RenderWhen from "../../../../Components/Common/RenderWhen";
-import { ADVANCE_PAYMENT, DUE_AMOUNT, INVOICE, OPD_BILL } from "./data";
+import { ADVANCE_PAYMENT, ALL_TRANSACTIONS, DUE_AMOUNT, INVOICE, OPD_BILL } from "./data";
 
 const Banner = ({ data, billType }) => {
   console.log(billType);
   const totalAdvancePayment = (dt) => {
     let amount = 0;
     dt?.forEach((item) => {
+      let baseAmount = 0;
+
       if (item.intern && item.receipt?.totalAmount) {
-        amount += item.receipt.totalAmount;
+        baseAmount += item.receipt.totalAmount;
       } else if (billType === DUE_AMOUNT) {
-        amount += item?.totalAdvancePayment || 0;
+        baseAmount += item?.totalAdvancePayment ?? 0;
+
       } else if (billType === OPD_BILL) {
-        amount += item?.receiptInvoice?.payable || 0;
+        baseAmount += item?.receiptInvoice?.payable ?? 0;
       } else {
-        amount +=
-          item?.advancePayment?.totalAmount ||
-          item?.receiptInvoice?.payable ||
+        baseAmount +=
+          item?.advancePayment?.totalAmount ??
+          item?.receiptInvoice?.payable ??
           0;
+      }
+      if (billType === ALL_TRANSACTIONS || billType === INVOICE) {
+        let refund = item?.invoice?.refund ?? 0;
+        amount += baseAmount - refund;
+      } else {
+        amount += baseAmount;
       }
     });
     return amount;
@@ -48,11 +57,21 @@ const Banner = ({ data, billType }) => {
     return amount;
   };
 
-  console.log(
-    data?.reduce((total, item) => total + (item.invoice?.payable || 0), 0),
-    "data",
-    data
-  );
+  const totalRefundAmount = (dt) => {
+    let refundTotal = 0;
+
+    dt?.forEach((item) => {
+      refundTotal += item?.invoice?.refund ?? 0;
+    });
+
+    return refundTotal;
+  };
+
+  // console.log(
+  //   data?.reduce((total, item) => total + (item.invoice?.payable || 0), 0),
+  //   "data",
+  //   data
+  // );
 
   const dueAmount = (dt) => {
     const totalAdPayment = totalAdvancePayment(dt);
@@ -77,6 +96,14 @@ const Banner = ({ data, billType }) => {
               <h6 className="display-6 fs-6">TOTAL PAID AMOUNT (₹): </h6>
               <h5 className="display-5 ms-2 fs-17 font-semi-bold">
                 {totalAdvancePayment(data) || 0.0}
+              </h5>
+            </div>
+          </RenderWhen>
+          <RenderWhen isTrue={billType === ALL_TRANSACTIONS || billType === INVOICE}>
+            <div className="d-flex align-items-center">
+              <h6 className="display-6 fs-6">TOTAL REFUND AMOUNT (₹): </h6>
+              <h5 className="display-5 ms-2 fs-17 font-semi-bold">
+                {totalRefundAmount(data) || 0.0}
               </h5>
             </div>
           </RenderWhen>
