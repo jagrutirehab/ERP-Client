bda1a0934a369b77c74370b20f12f2584f87614b
Hey! My Name is Sarang Padulkar <124503971+sarang136@users.noreply.github.com>
2026-01-29 15:43:29 +0530

    hiring pages

--- src/Components/Common/renderStatusBadge.js
+++ src/Components/Common/renderStatusBadge.js
@@ -1,164 +1,184 @@
 import { Badge } from "reactstrap";
 
 export const renderStatusBadge = (stage) => {
-    if (!stage) return "-";
+  if (!stage) return "-";
 
-    const badgeStyle = {
-        whiteSpace: "normal",
-        display: "inline-block",
-        lineHeight: "1.2",
-        textAlign: "center",
-    };
+  const badgeStyle = {
+    whiteSpace: "normal",
+    display: "inline-block",
+    lineHeight: "1.2",
+    textAlign: "center",
+  };
 
-    const map = {
-        EXIT_APPROVED: {
-            text: "Exit Approved",
-            color: "success",
-        },
-        EXIT_REJECTED: {
-            text: "Exit Rejected",
-            color: "danger",
-        },
-        EXIT_REJECTED_AND_ACTIVE_EMPLOYEE: {
-            text: "Exit Rejected & Active Employee",
-            color: "danger",
-        },
-        FNF_APPROVED: {
-            text: "FNF Approved",
-            color: "success",
-        },
-        FNF_REJECTED: {
-            text: "FNF Rejected",
-            color: "danger",
-        },
-        FNF_REJECTED_AND_ACTIVE_EMPLOYEE: {
-            text: "FNF Rejected & Active Employee",
-            color: "danger",
-        },
-        NONE: {
-            text: "None",
-            color: "warning"
-        },
-        WEEK_OFFS: {
-            text: "Week Off",
-            color: "info"
-        },
-        FESTIVE_LEAVE:{
-            text:"Festive Leave",
-            color:"info"
-        },
-        PENDING: {
-            text: "Pending",
-            color: "warning"
-        },
-        PRESENT: {
-            text: "Present",
-            color: "success"
-        },
-        ABSENT: {
-            text: "Absent",
-            color: "danger"
-        },
-        REGULARIZED: {
-            text: "Regularized",
-            color: "success"
-        },
-        APPROVED: {
-            text: "Approved",
-            color: "success"
-        },
-        REJECTED: {
-            text: "Rejected",
-            color: "danger"
-        },
-        EARNED_LEAVE: {
-            text: "Earned Leave",
-            color: "info"
-        },
-        LEAVE_WTIHOUT_PAYS: {
-            text: "Leave Without Pay",
-            color: "info"
-        },
-        FULL_DAY: {
-            text: "Full Day",
-            color: "info"
-        },
-        HALF_DAY: {
-            text: "HALF_DAY",
-            color: "info"
-        },
-        FIRST_HALF: {
-            text: "First Half",
-            color: "success"
-        },
-        SECOND_HALF: {
-            text: "Second Half",
-            color: "success"
-        },
-        REJECTED_AND_ACTIVE_EMPLOYEE: {
-            text: "Rejected & Active Employee",
-            color: "danger"
-        },
-        NEW_JOINING_PENDING: {
-            text: "New Joining Pending",
-            color: "warning"
-        },
-        NEW_JOINING_APPROVED_USER_CREATED: {
-            text: "New Joining Approved & User Created",
-            color: "success"
-        },
-        NEW_JOINING_REJECTED: {
-            text: "New Joining Rejected",
-            color: "danger"
-        },
-        EXIT_EMPLOYEE_PENDING: {
-            text: "Exit Employee Pending",
-            color: "warning"
-        },
-        EXIT_EMPLOYEE_APPROVED_USER_SUSPENDED: {
-            text: "Exit Employee Approved & User Suspended",
-            color: "success"
-        },
-        EXIT_EMPLOYEE_REJECTED: {
-            text: "Exit Employee Rejected",
-            color: "danger"
-        },
-        CURRENT_LOCATION_PENDING: {
-            text: "Current Location Pending",
-            color: "warning"
-        },
-        CURRENT_LOCATION_REJECTED: {
-            text: "Current Location Rejected",
-            color: "danger"
-        },
-        TRANSFER_LOCATION_PENDING: {
-            text: "Transfer Location Pending",
-            color: "warning"
-        },
-        TRANSFER_LOCATION_APPROVED: {
-            text: "Transfer Location Approved",
-            color: "success"
-        },
-        TRANSFER_LOCATION_REJECTED: {
-            text: "Transfer Location Rejected",
-            color: "danger"
-        },
-        TRANSFER_EMPLOYEE_PENDING: {
-            text: "Transfer Location Pending",
-            color: "warning"
-        },
-        TRANSFER_EMPLOYEE_APPROVED_USER_UPDATED: {
-            text: "Transfer Location Approved & User Updated",
-            color: "success"
-        },
-        TRANSFER_EMPLOYEE_REJECTED: {
-            text: "Transfer Location REJECTED",
-            color: "success"
-        }
-    };
+  const map = {
+    EXIT_APPROVED: {
+      text: "Exit Approved",
+      color: "success",
+    },
+    EXIT_REJECTED: {
+      text: "Exit Rejected",
+      color: "danger",
+    },
+    EXIT_REJECTED_AND_ACTIVE_EMPLOYEE: {
+      text: "Exit Rejected & Active Employee",
+      color: "danger",
+    },
+    FNF_APPROVED: {
+      text: "FNF Approved",
+      color: "success",
+    },
+    FNF_REJECTED: {
+      text: "FNF Rejected",
+      color: "danger",
+    },
+    FNF_REJECTED_AND_ACTIVE_EMPLOYEE: {
+      text: "FNF Rejected & Active Employee",
+      color: "danger",
+    },
+    NONE: {
+      text: "None",
+      color: "warning",
+    },
+    WEEK_OFFS: {
+      text: "Week Off",
+      color: "info",
+    },
+    FESTIVE_LEAVE: {
+      text: "Festive Leave",
+      color: "info",
+    },
+    PENDING: {
+      text: "Pending",
+      color: "warning",
+    },
+    PRESENT: {
+      text: "Present",
+      color: "success",
+    },
+    ABSENT: {
+      text: "Absent",
+      color: "danger",
+    },
+    REGULARIZED: {
+      text: "Regularized",
+      color: "success",
+    },
+    APPROVED: {
+      text: "Approved",
+      color: "success",
+    },
+    REJECTED: {
+      text: "Rejected",
+      color: "danger",
+    },
+    EARNED_LEAVE: {
+      text: "Earned Leave",
+      color: "info",
+    },
+    LEAVE_WTIHOUT_PAYS: {
+      text: "Leave Without Pay",
+      color: "info",
+    },
+    FULL_DAY: {
+      text: "Full Day",
+      color: "info",
+    },
+    HALF_DAY: {
+      text: "HALF_DAY",
+      color: "info",
+    },
+    FIRST_HALF: {
+      text: "First Half",
+      color: "success",
+    },
+    SECOND_HALF: {
+      text: "Second Half",
+      color: "success",
+    },
+    REJECTED_AND_ACTIVE_EMPLOYEE: {
+      text: "Rejected & Active Employee",
+      color: "danger",
+    },
+    NEW_JOINING_PENDING: {
+      text: "New Joining Pending",
+      color: "warning",
+    },
+    NEW_JOINING_APPROVED_USER_CREATED: {
+      text: "New Joining Approved & User Created",
+      color: "success",
+    },
+    NEW_JOINING_REJECTED: {
+      text: "New Joining Rejected",
+      color: "danger",
+    },
+    EXIT_EMPLOYEE_PENDING: {
+      text: "Exit Employee Pending",
+      color: "warning",
+    },
+    EXIT_EMPLOYEE_APPROVED_USER_SUSPENDED: {
+      text: "Exit Employee Approved & User Suspended",
+      color: "success",
+    },
+    EXIT_EMPLOYEE_REJECTED: {
+      text: "Exit Employee Rejected",
+      color: "danger",
+    },
+    CURRENT_LOCATION_PENDING: {
+      text: "Current Location Pending",
+      color: "warning",
+    },
+    CURRENT_LOCATION_REJECTED: {
+      text: "Current Location Rejected",
+      color: "danger",
+    },
+    TRANSFER_LOCATION_PENDING: {
+      text: "Transfer Location Pending",
+      color: "warning",
+    },
+    TRANSFER_LOCATION_APPROVED: {
+      text: "Transfer Location Approved",
+      color: "success",
+    },
+    TRANSFER_LOCATION_REJECTED: {
+      text: "Transfer Location Rejected",
+      color: "danger",
+    },
+    TRANSFER_EMPLOYEE_PENDING: {
+      text: "Transfer Location Pending",
+      color: "warning",
+    },
+    TRANSFER_EMPLOYEE_APPROVED_USER_UPDATED: {
+      text: "Transfer Location Approved & User Updated",
+      color: "success",
+    },
+    TRANSFER_EMPLOYEE_REJECTED: {
+      text: "Transfer Location REJECTED",
+      color: "success",
+    },
+    HOLD: {
+      text: "Hold",
+      color: "info",
+    },
+    CLOSED: {
+      text: "Closed",
+      color: "success",
+    },
+    NOT_STARTED_WORKING_YET: {
+      text: "Not Started",
+      color: "danger",
+    },
+    WIP: {
+      text: "WIP",
+      color: "warning",
+    },
+  };
 
-    const config = map[stage];
-    if (!config) return "-";
+  const config = map[stage];
+  if (!config) return "-";
 
-    return <Badge color={config.color} style={badgeStyle}>{config.text}</Badge>;
+  return (
+    <Badge color={config.color} style={badgeStyle}>
+      {config.text}
+    </Badge>
+  );
 };
--- src/pages/HR/Hiring/HiringManagement/index.js
+++ src/pages/HR/Hiring/HiringManagement/index.js
@@ -8,6 +8,13 @@ import DataTableComponent from "../../../HRMS/components/Table/DataTable";
 import EditHiringRequestModal from "../../components/EditHiringRequestModal";
 import { editManagementRequests } from "../../../../helpers/backend_helper";
 import { toast } from "react-toastify";
+import Select from "react-select";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDesignations } from "../../../../store/features/HR/hrSlice";
+import { useAuthError } from "../../../../Components/Hooks/useAuthError";
+import { HiringPreferredGenderOptions } from "../../../../Components/constants/HR";
+import { usePermissions } from "../../../../Components/Hooks/useRoles";
+import { useNavigate } from "react-router-dom";
 
 const HiringManagement = () => {
   const isMobile = useMediaQuery("(max-width: 1000px)");
@@ -16,13 +23,95 @@ const HiringManagement = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedRow, setSelectedRow] = useState(null);
   const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
+  const [filters, setFilters] = useState({
+    center: "ALL",
+    designation: null,
+    gender: null,
+    updateStatus: null,
+  });
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [spagination, setsPagination] = useState({
+    totalDocs: 0,
+  });
+  const handleAuthError = useAuthError();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.User);
+  const {
+    data,
+    pagination,
+    loader,
+    designations: designationOptions,
+    designationLoading,
+  } = useSelector((state) => state.HR);
+  const microUser = localStorage.getItem("micrologin");
+  const token = microUser ? JSON.parse(microUser).token : null;
+
+  const { hasPermission } = usePermissions(token);
+  const hasUserPermission = hasPermission("HR", "HIRING_MANAGEMENT", "READ");
+
+  const centerOptions = [
+    ...(user?.centerAccess?.length > 1
+      ? [
+          {
+            value: "ALL",
+            label: "All Centers",
+            isDisabled: false,
+          },
+        ]
+      : []),
+    ...(user?.centerAccess?.map((id) => {
+      const center = user?.userCenters?.find((c) => c._id === id);
+      return {
+        value: id,
+        label: center?.title || "Unknown Center",
+      };
+    }) || []),
+  ];
+
+  const selectedCenterOption =
+    centerOptions.find((opt) => opt.value === filters.center) ||
+    centerOptions[0];
+
+  const selectedGenderOption =
+    HiringPreferredGenderOptions.find((opt) => opt.value === filters.gender) ||
+    null;
+
+  const selectedDesignationOption =
+    designationOptions.find((opt) => opt.value === filters.designation) || null;
+
+  useEffect(() => {
+    if (
+      filters.center !== "ALL" &&
+      !user?.centerAccess?.includes(filters.center)
+    ) {
+      setFilters("ALL");
+      setPage(1);
+    }
+  }, [filters.center, user?.centerAccess]);
 
   const loadHiringManagementRequests = async () => {
     setLoading(true);
     try {
-      const response = await getManagementHiringRequests();
-      console.log("response", response);
+      const centers =
+        filters.center === "ALL"
+          ? user?.centerAccess
+          : !user?.centerAccess.length
+            ? []
+            : [filters.center];
+
+      const response = await getManagementHiringRequests({
+        page,
+        limit,
+        centers,
+        ...(filters.updateStatus ? { updateStatus: filters.updateStatus } : {}),
+        ...(filters.designation ? { designation: filters.designation } : {}),
+        ...(filters.gender ? { gender: filters.gender } : {}),
+      });
+      // console.log("response", response);
       setRequests(response?.data);
+      setsPagination(response?.pagination);
     } catch (error) {
       console.log(error);
     } finally {
@@ -31,8 +120,11 @@ const HiringManagement = () => {
   };
 
   useEffect(() => {
+    if (!hasUserPermission) {
+      navigate("/unauthorized");
+    }
     loadHiringManagementRequests();
-  }, []);
+  }, [filters, page, limit, user?.centerAccess]);
 
   const handleActionClick = (row) => {
     setSelectedRow(row);
@@ -59,21 +151,48 @@ const HiringManagement = () => {
       };
 
       const response = await editManagementRequests(selectedRow._id, payload);
-      console.log("response", response);
+      // console.log("response", response);
       if (response.success === true) {
-        toast.success(response?.message);
+        toast.success(response?.message || "Successfully updated!");
       }
+
       setIsModalOpen(false);
       loadHiringManagementRequests();
     } catch (error) {
       console.log(error);
-      toast.error("Error Updating Data");
+      toast.error(error?.message || "Error Updating Data");
     } finally {
       setLoading(false);
     }
   };
 
-  console.log("requests", requests);
+  useEffect(() => {
+    const loadDesignations = async () => {
+      try {
+        dispatch(
+          fetchDesignations({ status: ["PENDING", "APPROVED"] }),
+        ).unwrap();
+      } catch (error) {
+        if (!handleAuthError(error)) {
+          toast.error("Something went wrong while getting the designations");
+        }
+      }
+    };
+
+    loadDesignations();
+  }, []);
+
+  const UpdateStatusOptions = [
+    { value: "HOLD", label: "Hold" },
+    { value: "WIP", label: "Work In Progress" },
+    { value: "CLOSED", label: "Closed" },
+    { value: "NOT_STARTED_WORKING_YET", label: "Not Started Working" },
+  ];
+
+  const selectedUpdateStatusOption =
+    UpdateStatusOptions.find((opt) => opt.value === filters.updateStatus) ||
+    null;
+
   return (
     <CardBody
       className="p-3 bg-white"
@@ -82,10 +201,87 @@ const HiringManagement = () => {
       <div className="text-center text-md-left mb-4">
         <h1 className="display-6 fw-bold text-primary">HIRING MANAGEMENT</h1>
       </div>
+
+      <div className="d-flex flex-wrap gap-3 mb-3">
+        {/* Center */}
+        <div style={{ width: isMobile ? "100%" : "200px" }}>
+          <Select
+            options={centerOptions}
+            value={selectedCenterOption}
+            onChange={(opt) => {
+              setFilters((prev) => ({
+                ...prev,
+                center: opt?.value || "ALL",
+              }));
+              setPage(1);
+            }}
+            placeholder="All Centers"
+          />
+        </div>
+
+        <div style={{ width: isMobile ? "100%" : "180px" }}>
+          <Select
+            options={HiringPreferredGenderOptions}
+            value={selectedGenderOption}
+            onChange={(opt) => {
+              setFilters((prev) => ({
+                ...prev,
+                gender: opt ? opt.value : null,
+              }));
+              setPage(1);
+            }}
+            placeholder="Gender"
+            isClearable
+          />
+        </div>
+
+        {/* Designation */}
+        <div style={{ width: isMobile ? "100%" : "250px" }}>
+          <Select
+            options={designationOptions}
+            value={selectedDesignationOption}
+            onChange={(opt) => {
+              setFilters((prev) => ({
+                ...prev,
+                designation: opt ? opt.value : null,
+              }));
+              setPage(1);
+            }}
+            isLoading={designationLoading || loading}
+            placeholder="Designation"
+            isClearable
+          />
+        </div>
+
+        <div style={{ width: isMobile ? "100%" : "220px" }}>
+          <Select
+            options={UpdateStatusOptions}
+            value={selectedUpdateStatusOption}
+            onChange={(opt) => {
+              setFilters((prev) => ({
+                ...prev,
+                updateStatus: opt ? opt.value : null,
+              }));
+              setPage(1);
+            }}
+            placeholder="Update Status"
+            isClearable
+          />
+        </div>
+      </div>
+
       <DataTableComponent
         columns={HiringActionColumns({ onActionClick: handleActionClick })}
         data={requests}
         loading={loading}
+        pagination={pagination}
+        page={page}
+        setPage={setPage}
+        limit={limit}
+        setLimit={(newLimit) => {
+          setLimit(newLimit);
+          setPage(1);
+        }}
       />
 
       <EditHiringRequestModal
