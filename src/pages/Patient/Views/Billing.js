6520ca8afb40343af61a003c9ce500d6ac364ac4
Hey! My Name is Sarang Padulkar <124503971+sarang136@users.noreply.github.com>
2026-02-28 16:25:47 +0530

    write off bugs

--- src/pages/Patient/Views/Billing.js
+++ src/pages/Patient/Views/Billing.js
@@ -171,8 +171,11 @@ const Billing = ({
   const adjustedPayable = useMemo(() => {
     const latestId = latestAdmission?._id;
     const writeOff = Number(writeOffMap[latestId]) || 0;
-    const advance = Number(calculatedAdvance) || 0;
-    const payable = Number(calculatedPayable) || 0;
+    // const advance = Number(calculatedAdvance) || 0;
+    // const payable = Number(calculatedPayable) || 0;
+    const advance = Number(totalAdvance) || 0;
+    const payable = Number(totalPayable) || 0;
+
 
     return (advance + writeOff) - payable;
   }, [
@@ -181,6 +184,7 @@ const Billing = ({
     writeOffMap,
     latestAdmission?._id,
   ]);
+
 
 
   return (
